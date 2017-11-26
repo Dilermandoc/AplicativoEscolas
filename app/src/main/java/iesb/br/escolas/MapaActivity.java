c99ae7292a02805feeaf8a239d08bcb8dad28842
ingridy-id <ingridy-id@hotmail.com>
2017-11-26 17:52:30 -0200

    map

--- app/src/main/java/iesb/br/escolas/MapaActivity.java
+++ app/src/main/java/iesb/br/escolas/MapaActivity.java
@@ -31,7 +31,7 @@ public class MapaActivity extends AppCompatActivity implements OnMapReadyCallbac
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mapa);
 
 //        Intent intent = getIntent();
 //        String latitude = intent.getStringExtra("latitude");
